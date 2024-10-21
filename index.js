286e8d3a23b9ede8f6f969a17985989dc07f6faa
Gabriel Mota <107560699+gMota07@users.noreply.github.com>
2024-10-21 16:31:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+function abrir(){
+var botao = document.getElementsByTagName("botao")
+for (var i = 0; i < botoes.length; i++) {
+    botoes[i].click();
+    var mod = i % 2;
+    if (mod != 0) {
+        
+    }
+}}
+
